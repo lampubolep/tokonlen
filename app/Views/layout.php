22fd0829368c9e454756756e9412712e56cc1762
lampubolep <chieldesickn@gmail.com>
2021-03-02 23:25:57 +0700

    First Commit

---
+++ app/Views/layout.php
@@ -0,0 +1,52 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>Starter Template · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/starter-template/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?= base_url('/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="<?= base_url('/bootstrap/css/starter-template.css') ?>" rel="stylesheet">
+</head>
+<body>
+  <?= $this->include('navbar') ?>
+  <main class="container">
+
+      <?= $this->renderSection('content') ?>
+
+  </main><!-- /.container -->
+
+    <script src="<?= base_url('/bootstrap/js/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('/bootstrap/js/bootstrap.min.js') ?>"></script>
+    
+</body>
+</html>
